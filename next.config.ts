e5cbff03ddf51b98fe4de4758bc52f8017778b55
shreneeka <shreneeka.scc@gmail.com>
2026-08-03 11:33:03 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
  
-  allowedDevOrigins: ['192.168.1.18'],
+  allowedDevOrigins: ['192.168.1.19'],
+  // Explicitly set the Turbopack root to the current directory
+  // to resolve the "inferred workspace root" warning.
+  turbopack: { root: __dirname },
 
 };
 
